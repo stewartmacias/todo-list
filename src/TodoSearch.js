5f0bc3ef9b805f6b48fa64b8a9cffd93aeca00cd
stewartmacias <macias.stewart@gmail.com>
2021-11-28 11:35:34 -0500

    JSX, styles

---
+++ src/TodoSearch.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './TodoSearch.css';
+
+function TodoSearch( props ) {
+    return (
+        <input className="TodoSearch" placeholder="Cebolla"/>
+    );
+}
+
+export { TodoSearch };
