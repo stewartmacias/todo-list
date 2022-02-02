217841b91d355042ed38cd4eb2c3b445e7df9e27
Stewart Macias <smacias@technisys.com>
2022-02-02 11:01:06 -0500

    Composición y colocación del estado en React

--- src/App/index.js
+++ src/App/index.js
@@ -3,12 +3,55 @@ import { TodoProvider} from "../TodoContext";
 import { AppUI } from "./AppUI";
 
 function App() {
+  const [state, setState] = React.useState('estado compartido');
+  return (
+    <React.Fragment>
+      <TodoHeader >
+        <TodoCounter />
+        <TodoSearch />
+      </TodoHeader>
+      <TodoList>
+        <TodoItem state={state}/>
+      </TodoList>
+  </React.Fragment>
+  );
+}
+
+function TodoHeader( {children} ) {
+  return (
+    <header>
+      {children}
+    </header>
+  );
+}
+
+function TodoList( {children} ) {
+  return (
+    <section className="todoList-container">
+      {children}
+    </section>
+  );
+}
+
+function TodoCounter() {
+  return <p>TodoCounter</p>;
+}
+
+function TodoSearch() {
+  return <p>TodoSearch</p>;
+}
+
+function TodoItem( {state} ) {
+  return <p>TodoItem: {state}</p>;
+}
+
+/* function App() {
   
   return (
     <TodoProvider>
       <AppUI />
     </TodoProvider>
   );
-}
+} */
 
 export default App;
